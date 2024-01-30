a36f67ed8b2a122d8fcf87a30a888470090571ff
Abdelhalim Samy <me@halimsamy.com>
2024-01-30 22:19:50 +0200

    feat: define a seeder instanse exported by default

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,7 @@ import Seeder from './seeder';
 import SeederFactory from './factory';
 import { ref } from './ref';
 
+const Graine = new Seeder();
+
+export default Graine;
 export { ISeederWriter, Seeder, SeederFactory, ref };
